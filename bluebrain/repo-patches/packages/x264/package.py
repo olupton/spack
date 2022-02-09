b473aaf5d8d52369523b137eec7ffcc7c101ab7f
Olli Lupton <oliver.lupton@epfl.ch>
2022-02-09 09:09:57 +0100

    updated x264 recipe

--- bluebrain/repo-patches/packages/x264/package.py
+++ bluebrain/repo-patches/packages/x264/package.py
@@ -1,20 +1,19 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-from spack import *
+from spack.directives import depends_on, version
 
 
 class X264(AutotoolsPackage):
     """Software library and application for encoding video streams"""
 
     homepage = "https://www.videolan.org/developers/x264.html"
-    git     = "https://code.videolan.org/videolan/x264.git"
+    git = "https://code.videolan.org/videolan/x264.git"
 
-    version('r2984', commit='3759fcb7b48037a5169715ab89f80a0ab4801cdf')
+    version("20210613", commit="5db6aa6cab1b146e07b60cc1736a01f21da01154")
 
-    depends_on('nasm')
+    depends_on("nasm")
 
     def configure_args(self):
-        return ['--enable-shared', '--enable-pic']
+        return ["--enable-shared", "--enable-pic"]
